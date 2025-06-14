8dc30cbd3ef487d582c445059574756e2ee626d3
Ketan <ketan.darekar99@gmail.com>
2025-06-14 10:33:26 +0200

    Create Navbar

--- frontend/src/component/Navbar/Navbar.jsx
+++ frontend/src/component/Navbar/Navbar.jsx
@@ -1,7 +1,45 @@
 import React from 'react'
+import { Badge, IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import {Avatar} from '@mui/material'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import "./Navbar.css"
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='PX-5 z-50 py-[.8rem] bg-[#add8e6] lg:px-20 flex justify-between'>
+        <div className='lg:mr-10 cursor-pointer flex items-center space-x-4'>
+          <li className='logo font-semibold text-black-300 text-2xl'>
+            BITEAWAY
+          </li>
+
+        </div>
+      <div className='flex items-center space-x-2 lg:space-x-10'>
+        <div className=''>
+          <IconButton>
+            <SearchIcon SX={{fontSize:"1.5rem"}}/>
+          </IconButton>
+
+        </div>
+        <div className=''>
+          <Avatar sx={{bgcolor:"gray"}}>K</Avatar>
+
+        </div>
+
+        <div className=''>
+          <IconButton>
+            <Badge color='secondary' badgeContent={2}>
+              <ShoppingCartIcon SX={{fontSize:"1.5rem"}}/>
+
+            </Badge>
+            
+          </IconButton>
+          
+
+        </div>
+
+      </div>
+
+    </div>
   )
 }
